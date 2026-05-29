3939516eca0e6c4f45ecfa656b50830ab86b1a9f
Zaaabinski <jakub.zlobinski03@gmail.com>
2026-05-29 17:13:23 +0200

    Added audio to scenes

--- Assets/Scripts/MenuVideoScript.cs
+++ Assets/Scripts/MenuVideoScript.cs
@@ -5,11 +5,18 @@ public class MenuVideoScript : MonoBehaviour
 {
    private VideoPlayer videoPlayer;
    [SerializeField] private GameObject videoScreen;
+   [SerializeField] private GameObject soundPlayer;
 
     private void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
+        Invoke("TurnSoundOn", (float)videoPlayer.clip.length-2.7f);
         Destroy(videoScreen, (float)videoPlayer.clip.length-2.7f);
         Destroy(gameObject, (float)videoPlayer.clip.length-2.7f);
+    }
+
+    private void TurnSoundOn()
+    {
+        soundPlayer.SetActive(true);
     }
 }
